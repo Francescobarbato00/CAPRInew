37d92851003d3414aea37a72a9fb99b17232a417
Francescobarbato00 <francesco.barbato24@gmail.com>
2024-10-13 11:44:48 +0200

    a

--- src/pages/components/Hero.jsx
+++ src/pages/components/Hero.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { supabase } from '../api/supabaseClient';  // Assicurati di importare il client di Supabase
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 
 export default function HeroSection() {
@@ -13,6 +15,20 @@ export default function HeroSection() {
     gruppoTematico: '',
     note: '',
   });
+
+  const [user, setUser] = useState(null);  // Stato per tenere traccia dell'utente autenticato
+  const router = useRouter();
+
+  // Verifica se l'utente è autenticato all'inizio
+  useEffect(() => {
+    const getUser = async () => {
+      const { data, error } = await supabase.auth.getUser();
+      if (data?.user) {
+        setUser(data.user);  // Imposta l'utente se è autenticato
+      }
+    };
+    getUser();
+  }, []);
 
   // Gestisce il cambiamento degli input del form
   const handleChange = (e) => {
@@ -45,6 +61,13 @@ export default function HeroSection() {
       console.error('Errore durante l\'invio dei dati:', error);
       alert('Errore durante l\'invio dei dati');
     }
+  };
+
+  // Funzione per il logout
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    setUser(null);
+    router.push('/login');  // Reindirizza alla pagina di login dopo il logout
   };
 
   return (
@@ -92,171 +115,180 @@ export default function HeroSection() {
             Capri 2025
           </h3>
 
-          {/* Bottone per aprire il form */}
-          <div className="text-center" onClick={() => setShowModal(true)}>
-            <div className="w-12 h-12 mb-4 bg-blue-700 rounded-full flex justify-center items-center mx-auto transform transition-transform duration-300 hover:scale-110 cursor-pointer shadow-lg">
-              <span className="text-3xl text-white">+</span>
+          {/* Se l'utente è autenticato, mostra il form di iscrizione */}
+          {user ? (
+            <div className="text-center">
+              <div className="w-12 h-12 mb-4 bg-blue-700 rounded-full flex justify-center items-center mx-auto transform transition-transform duration-300 hover:scale-110 cursor-pointer shadow-lg" onClick={() => setShowModal(true)}>
+                <span className="text-3xl text-white">+</span>
+              </div>
+              <h2 className="text-lg md:text-xl font-bold mb-2" style={{ fontWeight: 600 }}>
+                Iscriviti al Convegno
+              </h2>
+              <p className="text-sm md:text-base" style={{ fontWeight: 400 }}>
+                L'adesione al convegno dovrà essere effettuata tramite la compilazione del form
+              </p>
+
+              
             </div>
-            <h2
-              className="text-lg md:text-xl font-bold mb-2"
-              style={{ fontWeight: 600 }}
-            >
-              Iscriviti al Convegno
-            </h2>
-            <p className="text-sm md:text-base" style={{ fontWeight: 400 }}>
-              L'adesione al convegno dovrà essere effettuata tramite la compilazione del form
-            </p>
-          </div>
+          ) : (
+            <div className="text-center">
+              <h2 className="text-lg md:text-xl font-bold mb-2" style={{ fontWeight: 600 }}>
+                Accedi per iscriverti al convegno
+              </h2>
+              <button onClick={() => router.push('/login')} className="bg-blue-500 text-white py-2 px-4 rounded-md">
+                Login
+              </button>
+            </div>
+          )}
         </div>
       </section>
 
-      {/* Modal del form con stile migliorato */}
-      {showModal && (
-  <div className="fixed inset-0 flex items-center justify-center z-50">
-    <div className="bg-white rounded-lg shadow-2xl w-full max-w-xl p-6 relative z-20 transition-transform duration-300 transform mx-4 md:mx-auto overflow-auto" style={{ maxHeight: '90vh' }}>
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-2xl font-bold text-gray-800">Iscriviti al Convegno</h2>
-        <button
-          className="text-gray-500 hover:text-gray-700"
-          onClick={() => setShowModal(false)}
-        >
-          ✖
-        </button>
-      </div>
+      {/* Modal del form con stile migliorato, visibile solo per utenti autenticati */}
+      {showModal && user && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-white rounded-lg shadow-2xl w-full max-w-xl p-6 relative z-20 transition-transform duration-300 transform mx-4 md:mx-auto overflow-auto" style={{ maxHeight: '90vh' }}>
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-2xl font-bold text-gray-800">Iscriviti al Convegno</h2>
+              <button
+                className="text-gray-500 hover:text-gray-700"
+                onClick={() => setShowModal(false)}
+              >
+                ✖
+              </button>
+            </div>
 
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label className="block text-gray-700">Cognome</label>
-          <input
-            name="cognome"
-            type="text"
-            value={formData.cognome}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
-            placeholder="Inserisci il cognome"
-            required
-          />
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div>
+                <label className="block text-gray-700">Cognome</label>
+                <input
+                  name="cognome"
+                  type="text"
+                  value={formData.cognome}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
+                  placeholder="Inserisci il cognome"
+                  required
+                />
+              </div>
+
+              <div>
+                <label className="block text-gray-700">Nome</label>
+                <input
+                  name="nome"
+                  type="text"
+                  value={formData.nome}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
+                  placeholder="Inserisci il nome"
+                  required
+                />
+              </div>
+
+              <div>
+                <label className="block text-gray-700">Qualifica</label>
+                <select
+                  name="qualifica"
+                  value={formData.qualifica}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
+                  required
+                >
+                  <option value="">Seleziona la qualifica</option>
+                  <option value="MAGISTRATO ORDINARIO">Magistrato Ordinario</option>
+                  <option value="MAGISTRATO ONORARIO">Magistrato Onorario</option>
+                  <option value="MAGISTRATO AMMINISTRATIVO">Magistrato Amministrativo</option>
+                  <option value="MAGISTRATO TRIBUTARIO">Magistrato Tributario</option>
+                  <option value="MAGISTRATO DELLA CORTE DEI CONTI">Magistrato della Corte dei Conti</option>
+                  <option value="INFORMATICO">Informatico</option>
+                  <option value="AMMINISTRATIVO">Amministrativo</option>
+                  <option value="AVVOCATO LIBERO FORO">Avvocato Libero Foro</option>
+                  <option value="AVVOCATURA DELLO STATO">Avvocatura dello Stato</option>
+                  <option value="PROFESSORE UNIVERSITARIO">Professore Universitario</option>
+                  <option value="Altro">Altro</option>
+                </select>
+              </div>
+
+              <div>
+                <label className="block text-gray-700">Indirizzo Mail</label>
+                <input
+                  name="email"
+                  type="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
+                  placeholder="Inserisci la mail"
+                  required
+                />
+              </div>
+
+              <div>
+                <label className="block text-gray-700">Numero di Telefono</label>
+                <input
+                  name="telefono"
+                  type="tel"
+                  value={formData.telefono}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
+                  placeholder="Inserisci il numero di telefono"
+                  required
+                />
+              </div>
+
+              <div>
+                <label className="block text-gray-700">Sessione</label>
+                <select
+                  name="sessione"
+                  value={formData.sessione}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
+                  required
+                >
+                  <option value="4 ottobre">Sessione del 4 ottobre</option>
+                  <option value="5 ottobre">Sessione del 5 ottobre</option>
+                  <option value="nessuna">Nessuna</option>
+                </select>
+              </div>
+
+              <div>
+                <label className="block text-gray-700">Gruppo Tematico</label>
+                <select
+                  name="gruppoTematico"
+                  value={formData.gruppoTematico}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
+                >
+                  <option value="1">L’innovazione e la digitalizzazione del processo tributario</option>
+                  <option value="2">L’innovazione e la digitalizzazione nel diritto e processo civile</option>
+                  <option value="3">L’innovazione e la digitalizzazione del processo penale</option>
+                  <option value="4">Casi d'uso dell'intelligenza artificiale nella giustizia</option>
+                  <option value="nessuno">A nessuno dei gruppi</option>
+                </select>
+              </div>
+
+              <div>
+                <label className="block text-gray-700">Note</label>
+                <textarea
+                  name="note"
+                  value={formData.note}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
+                  placeholder="Inserisci eventuali note"
+                ></textarea>
+              </div>
+
+              <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-blue-600 transition-colors duration-300"
+              >
+                Invia
+              </button>
+            </form>
+          </div>
+
+          {/* Overlay scuro dietro il modal */}
+          <div className="fixed inset-0 bg-gray-900 opacity-60 z-10"></div>
         </div>
-
-        <div>
-          <label className="block text-gray-700">Nome</label>
-          <input
-            name="nome"
-            type="text"
-            value={formData.nome}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
-            placeholder="Inserisci il nome"
-            required
-          />
-        </div>
-
-        <div>
-          <label className="block text-gray-700">Qualifica</label>
-          <select
-            name="qualifica"
-            value={formData.qualifica}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
-            required
-          >
-            <option value="">Seleziona la qualifica</option>
-            <option value="MAGISTRATO ORDINARIO">Magistrato Ordinario</option>
-            <option value="MAGISTRATO ONORARIO">Magistrato Onorario</option>
-            <option value="MAGISTRATO AMMINISTRATIVO">Magistrato Amministrativo</option>
-            <option value="MAGISTRATO TRIBUTARIO">Magistrato Tributario</option>
-            <option value="MAGISTRATO DELLA CORTE DEI CONTI">Magistrato della Corte dei Conti</option>
-            <option value="INFORMATICO">Informatico</option>
-            <option value="AMMINISTRATIVO">Amministrativo</option>
-            <option value="AVVOCATO LIBERO FORO">Avvocato Libero Foro</option>
-            <option value="AVVOCATURA DELLO STATO">Avvocatura dello Stato</option>
-            <option value="PROFESSORE UNIVERSITARIO">Professore Universitario</option>
-            <option value="Altro">Altro</option>
-          </select>
-        </div>
-
-        <div>
-          <label className="block text-gray-700">Indirizzo Mail</label>
-          <input
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
-            placeholder="Inserisci la mail"
-            required
-          />
-        </div>
-
-        <div>
-          <label className="block text-gray-700">Numero di Telefono</label>
-          <input
-            name="telefono"
-            type="tel"
-            value={formData.telefono}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
-            placeholder="Inserisci il numero di telefono"
-            required
-          />
-        </div>
-
-        <div>
-          <label className="block text-gray-700">Sessione</label>
-          <select
-            name="sessione"
-            value={formData.sessione}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
-            required
-          >
-            <option value="4 ottobre">Sessione del 4 ottobre</option>
-            <option value="5 ottobre">Sessione del 5 ottobre</option>
-            <option value="nessuna">Nessuna</option>
-          </select>
-        </div>
-
-        <div>
-          <label className="block text-gray-700">Gruppo Tematico</label>
-          <select
-            name="gruppoTematico"
-            value={formData.gruppoTematico}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
-          >
-            <option value="1">L’innovazione e la digitalizzazione del processo tributario</option>
-            <option value="2">L’innovazione e la digitalizzazione nel diritto e processo civile</option>
-            <option value="3">L’innovazione e la digitalizzazione del processo penale</option>
-            <option value="4">Casi d'uso dell'intelligenza artificiale nella giustizia</option>
-            <option value="nessuno">A nessuno dei gruppi</option>
-          </select>
-        </div>
-
-        <div>
-          <label className="block text-gray-700">Note</label>
-          <textarea
-            name="note"
-            value={formData.note}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-black"
-            placeholder="Inserisci eventuali note"
-          ></textarea>
-        </div>
-
-        <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-blue-600 transition-colors duration-300"
-        >
-          Invia
-        </button>
-      </form>
-    </div>
-
-    {/* Overlay scuro dietro il modal */}
-    <div className="fixed inset-0 bg-gray-900 opacity-60 z-10"></div>
-  </div>
-)}
-
+      )}
     </>
   );
 }
