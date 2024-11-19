091657a39f4da4e70fe544dff73a7a6f6f94f898
Francescobarbato00 <francesco.barbato24@gmail.com>
2024-11-19 11:40:47 +0100

    a

--- src/pages/streaming.js
+++ src/pages/streaming.js
@@ -1,13 +1,14 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Storia from './components/storia';
+import ChatComponent from './components/ChatComponent';
 
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <ChatComponent />
+      < ChatComponent />
       <Storia />
       <Footer/>
     </div>
